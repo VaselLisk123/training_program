9defe3bd004334c5fef2643e998ce87701ce302f
Stanislav <vasell.lisk@gmail.com>
2022-07-19 17:26:40 +0300

    updated

--- parsing_excel_file.py
+++ parsing_excel_file.py
@@ -1,6 +1,3 @@
-from importlib.abc import ResourceLoader
-from tkinter import E
-from tokenize import blank_re
 import pandas as pd
 import config
 import shopify
@@ -40,13 +37,14 @@ def create_product(excel_info):
             variant_existance = False
         if variant_existance == False:
             new_product = shopify.Product.create({
-            "title": "test",
+            "title": "Default Title",
             "product_type": str(row["Product Type"]),
             "vendor": str(row["Brand"]),
             "tags": str(row["Category"]),
             })
+            current_product_id = new_product.get_id()
             new_product = shopify.Variant.create({
-            "product_id": new_product.get_id(),
+            "product_id": current_product_id,
             "barcode": str(row["Barcode"]),
             "sku": str(row["CentreSoft Code"]),
             "price": str(row["Trade (Inc Vat)"]),
@@ -54,22 +52,28 @@ def create_product(excel_info):
             "weight": row["WGHTA (KG)"],
             "option1": "Title",
             })
-            inventory_item = new_product.inventory_item_id
+            inventory_item_id = new_product.inventory_item_id
+            inventory_item = shopify.InventoryItem.find(inventory_item_id)
+            inventory_item.harmonized_system_code = str(row["Commodity Code"]).strip()
+            inventory_item.country_code_of_origin = str(row["Country Code"])
+            inventory_item.save()
             default_location_id = shopify.Shop.current().primary_location_id
             shopify.InventoryLevel.set(
                 location_id=default_location_id,
-                inventory_item_id=inventory_item,
+                inventory_item_id=inventory_item_id,
                 available=row["Carton Qty"],
-            )       
+            )
+            image = shopify.Image.create({
+            "product_id": current_product_id,
+            "src": str(row["IMAGE_URL"])
+            })
             for key in metafield_keys:
-                product_metafield = shopify.Metafield({
+                product_metafield = shopify.Metafield.create({
+                    "namespace": "trm_test",
                     "key": key,
                     "value": str(row[key]),
                     "type": "string",
-                    "namespace": "trm_test",
                 })
-                new_product.add_metafield(product_metafield)
-                new_product.save()
         else:
             variant.price = str(row["Trade (Inc Vat)"])
             inventory_item = variant.inventory_item_id
